f388ad2a69f828487f04b4613cdd8da544abfa42
martin <t-schiemann22@web.de>
2023-12-10 20:41:20 +0100

    Finished

--- Textstatistik.cpp
+++ Textstatistik.cpp
@@ -52,15 +52,13 @@ std::string Textstatistik::formateText(std::string& _text) {
 
     transform(_text.begin(), _text.end(), _text.begin(), ::tolower);
 
+    _text.erase(std::remove_if(_text.begin(), _text.end(), [](char c) {
+        return (!std::isalpha(c) &&  c != ' ' );
+    }), _text.end());
+
     removeDoubleSpaces(_text);
 
 
-    /*for (size_t t = 0; t < _text.length(); t++) {
-        if (isalpha(_text[t])) {
-            if (isupper(_text[t]))
-                _text[t] = _text[t] ^ 32;
-        }
-    }*/
     return _text;
 }
 
@@ -74,8 +72,8 @@ void Textstatistik::removeDoubleSpaces(std::string& text) {
 void Textstatistik::count(const string& _text) {
     std::unordered_map<char, int> letters;
     for (int i = 0; _text[i]; i++) {
-        if(::isalpha(text[i])) {
-            if (letters.find(text[i]) == letters.end()) {
+        if(isalpha(_text[i]) && _text[i] != ' ') {
+            if (letters.find(_text[i]) == letters.end()) {
                 letters.insert(make_pair(_text[i], 1));
 
             } else {
@@ -114,46 +112,31 @@ void Textstatistik::collectWordDialog() {
             continue;
         }else{
             n = stoi(input);
-            collect(formattedText, words);
+            //collect(formattedText, words);
+            createTable(words);
         }
     }
 }
 
 void Textstatistik::collect(string& _formattedText, vector<set<string>>& _words /*std::unordered_map<std::string, int>& _words*/) {
-    cout << _formattedText << endl;
+    //cout << _formattedText << endl;
     size_t count = 0;
-       /*while( _formattedText.length() > 1){
-            string word = extractWord(_formattedText);
-            if(_words.find(word) == _words.end()){
-                _words.insert(make_pair(word, 1));
-            }else{
-                _words[word]++;
-            }
-            count = 0;
-       }*/
-
-       /*while(_formattedText.length() > 1){
-           string word = extractWord(_formattedText);
-           int n = word.length();
-           if(n > 2 && n < 20){
-               _words[n].insert(word);
-           }
-       }*/
 
     while (_formattedText.length() > 1) {
         std::string word = extractWord(_formattedText);
         int size = word.length();
+
         if (size >= 2 && size <= 20) {
-            // Wenn die Länge des Worts in einem gültigen Bereich liegt, füge es zum entsprechenden Set hinzu
+
             if (size > _words.size()) {
-                // Stelle sicher, dass der Vektor groß genug ist
+
                 _words.resize(size);
             }
             _words[size - 3].insert(word);
         }
     }
 
-       consoleLogWords(_words);
+       //consoleLogWords(_words);
        //cout << "formatiert text collect words: " << _formattedText << endl;
        createTable(_words/*, _outputFile*/);
 }
@@ -175,7 +158,7 @@ std::string Textstatistik::extractWord(std::string& _formattedText) {
     bool space = true;
     while(space){
         //for(size_t i = 0; i < _formattedText.length(); i++) {
-            if (_formattedText[count] == 32 && count >= 2) {
+            if (_formattedText[count] == 32 && count >= 3) {
                 pushBackVar = _formattedText.substr(0, count);
                 _formattedText.erase(0, count );
                 space = false;
@@ -183,6 +166,7 @@ std::string Textstatistik::extractWord(std::string& _formattedText) {
                 count++;
             }
         }
+
     return pushBackVar ;
 }
 
@@ -197,26 +181,13 @@ void Textstatistik::createTable(vector<set<string>> _words /*const std::unordere
     vector<set<string>> tmp = _words;
 
     // Tabellenausgabe in die Datei
-    /*_outputFile << "+----------------+-------------+" << std::endl;
-    _outputFile << "|   words        |  Anzahl " << "(" << n << ")" << " |" << std::endl;
-    _outputFile << "+----------------+-------------+" << std::endl;
-    for (auto& it : tmp) {
-        //int count = wordLengthCount[n];
-        if (it.second == n || n == 0) {
-            _outputFile << "|" << setw(16) << it.first << "|   " << it.second << "         |" << std::endl;
-        }
-    }
-    _outputFile << "+----------------+-------------+" << std::endl;*/
-
-
-
     size_t size = count(tmp);
 
     _outputFile << "+----------------+-------------+" << std::endl;
     _outputFile << "| wordsize " << "(" << n << ")" << "   |  Anzahl (" << size << ")  |" << std::endl;
     _outputFile << "+----------------+-------------+" << std::endl;
 
-    if(n == 0) {
+    if(n == 0 || n == 1) {
         _outputFile << "|                |" << setw(16)  << "|" << endl;
     }else if(n > _words.size()){
         _outputFile << "|" << 0 << std::endl;
@@ -225,9 +196,6 @@ void Textstatistik::createTable(vector<set<string>> _words /*const std::unordere
             _outputFile << "|" << t << std::endl;
         }
     }
-
-    /*_outputFile << "|" << setw(16) << "|   " << size << "         |" << std::endl;
-    _outputFile << "+----------------+-------------+" << std::endl;*/
 
     // Schließen der Datei
     _outputFile.close();
--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 
 using namespace std;
 
+std::string entferneNichtBuchstaben(const std::string &eingabe) {
+    std::string bereinigterString;
+
+    for (char zeichen : eingabe) {
+        if (std::isalpha(zeichen)) {
+            bereinigterString += zeichen;
+        }
+    }
+
+    return bereinigterString;
+}
+
 int main(int argc, char* argv[]) {
 
+
     try {
-        //Textstatistik ts("/home/martin/Dokumente/studium_hsos/prog3/Textstatistik/Textfiles/glocke"); /*Wieso muss der ganze Pfad angegeben werden ?*/
-        Textstatistik ts("/home/martin/Dokumente/studium_hsos/prog3/Textstatistik/Textfiles/bsp.txt");
+        Textstatistik ts("/home/martin/Dokumente/studium_hsos/prog3/Textstatistik/Textfiles/glocke"); /*Wieso muss der ganze Pfad angegeben werden ?*/
+        //Textstatistik ts("/home/martin/Dokumente/studium_hsos/prog3/Textstatistik/Textfiles/bsp.txt");
+
         ts.determineFrequency();
 
 
@@ -17,6 +31,8 @@ int main(int argc, char* argv[]) {
     }catch (const runtime_error& ex){
         return 1;
     }
+
+
 
     return 0;
 }
